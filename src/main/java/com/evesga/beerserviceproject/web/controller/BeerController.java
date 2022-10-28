6c393beafaf302ef5a66d12af78e557ae1e01255
Edwin Vesga <evesga@Edwins-MacBook-Air.local>
2022-10-27 19:00:15 -0500

    fix controller

--- src/main/java/com/evesga/beerserviceproject/web/controller/BeerController.java
+++ src/main/java/com/evesga/beerserviceproject/web/controller/BeerController.java
@@ -13,7 +13,7 @@ public class BeerController {
 
     @GetMapping("/{beerId}")
     public ResponseEntity<BeerDto> getBeerById(@PathVariable("beerId") UUID beerId) {
-        return new ResponseEntity<>(BeerDto.builder().build(), , HttpStatus.OK);
+        return new ResponseEntity<>(BeerDto.builder().build(), HttpStatus.OK);
     }
 
     @PostMapping
